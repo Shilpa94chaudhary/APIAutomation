de8d8328ae00db00e79ab9b307e805ab747be19a
ShilpaGamezop <shilpa.94chaudhary@gmail.com>
2025-09-02 12:31:15 +0530

    RestAssured test: Get request

---
+++ src/test/java/herokuapp/restfulbooker/GetBookingIDsTest.java
@@ -0,0 +1,22 @@
+package herokuapp.restfulbooker;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+public class GetBookingIDsTest {
+    @Test
+    public void getBookingIDsWIthoutFilterTest(){
+        // Get response with booking IDs
+        Response response = RestAssured.get("https://restful-booker.herokuapp.com/booking");
+        // response.print();
+        // Verify response 200
+        Assert.assertEquals(response.statusCode(),200,"Status code should be 200, but it is not.");
+        // Verify at least 1 booking id in response
+        List<Integer> bookingIds = response.jsonPath().getList("bookingid");
+        Assert.assertFalse(bookingIds.isEmpty(),"List of booking IDs is empty but it should not be.");
+    }
+}
---
+++ src/test/java/herokuapp/restfulbooker/GetBookingTest.java
@@ -0,0 +1,46 @@
+package herokuapp.restfulbooker;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+public class GetBookingTest {
+    @Test
+    public void getBookingDetailTest(){
+        // Get booking detail for given booking ID
+        Response response = RestAssured.get("https://restful-booker.herokuapp.com/booking/9");
+        // Print response
+        response.print();
+        // Verify the status code
+        Assert.assertEquals(response.statusCode(),200,"Status code should be 200, but it is not");
+
+        // Verify all fields
+        // {"firstname":"Jim","lastname":"Ericsson","totalprice":341,
+        // "depositpaid":false,
+        // "bookingdates":{"checkin":"2019-04-24","checkout":"2023-09-26"},
+        // "additionalneeds":"Breakfast"}
+        // Get all values from response and save in variable
+        String firstName = response.jsonPath().getString("firstname");
+        String lastName = response.jsonPath().getString("lastname");
+        int totalPrice = response.jsonPath().getInt("totalprice");
+        Boolean depositPaid = response.jsonPath().getBoolean("depositpaid");
+        String checkIn = response.jsonPath().getString("bookingdates.checkin");
+        String checkOut = response.jsonPath().getString("bookingdates.checkout");
+        String additionalNeeds = response.jsonPath().getString("additionalneeds");
+
+        // Soft Assert
+        SoftAssert softAssert = new SoftAssert();
+        softAssert.assertEquals(firstName,"Jim","First name should be Jim, but it is not.");
+        softAssert.assertEquals(lastName,"Ericsson","Last name should be Ericsson, but it is not.");
+        softAssert.assertEquals(totalPrice,341,"Total price should be 341, but it is not.");
+        softAssert.assertFalse(depositPaid,"Deposit paid should be true, but it is not.");
+        softAssert.assertEquals(checkIn,"2019-04-24");
+        softAssert.assertEquals(checkOut,"2023-09-26");
+        softAssert.assertEquals(additionalNeeds,"Breakfast");
+
+        softAssert.assertAll();
+
+    }
+}
